06a6ebeba6a411884df90dbd9bd993c992323cec
Rodrigo <ti.rodrigoalmeida@gmail.com>
2019-04-26 23:06:50 -0300

    Mapeando perfis e permissões

---
+++ app/Models/Perfil.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Perfil extends Model
+{
+    use SoftDeletes;
+
+    protected $revisionCreationsEnabled = true;
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    protected $table = 'perfis';
+
+    protected $casts = [
+        'admin' => 'boolean'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome', 'nivel'
+    ];
+
+    /*
+     * Relacionamentos
+     */
+
+    public function permissoes()
+    {
+        return $this->belongsToMany(
+            Permissao::class, 'perfis_permissoes', 'perfil_id', 'permissao_id');
+    }
+
+    /*
+     * Helpers
+     */
+
+    public function syncPermissoes($permissoesEnviadas)
+    {
+        if (!auth()->user()->ehAdmin()) {
+            if (auth()->user()->perfil && auth()->user()->perfil->permissoes()->count()) {
+                $perms = [];
+                $permissoes = auth()->user()->perfil->permissoes()->get();
+                foreach ($permissoes as $permissao) {
+                    if (in_array($permissao->id, $permissoesEnviadas)) {
+                        $perms[] = $permissao->id;
+                    }
+                }
+                $this->permissoes()->sync($perms);
+            } else {
+                $this->permissoes()->sync([]);
+            }
+        } else {
+            $this->permissoes()->sync($permissoesEnviadas);
+        }
+    }
+}
---
+++ database/migrations/2019_04_27_013203_create_perfis_permissoes_tables.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerfisPermissoesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->boolean('admin')->default(false);
+            $table->tinyInteger('nivel');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('usuarios', function (Blueprint $table) {
+            $table->unsignedInteger('perfil_id')->after('id')->nullable();
+            $table->foreign('perfil_id')->references('id')->on('perfis')->onUpdate('cascade')->onDelete('set null');
+        });
+
+        Schema::create('permissoes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('tag', 50)->unique();
+        });
+
+        Schema::create('perfis_permissoes', function (Blueprint $table) {
+            $table->unsignedInteger('perfil_id');
+            $table->unsignedInteger('permissao_id');
+
+            $table->foreign('perfil_id')->references('id')->on('perfis')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('permissao_id')->references('id')->on('permissoes')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['perfil_id', 'permissao_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfis_permissoes');
+        Schema::dropIfExists('permissoes');
+
+        Schema::table('usuarios', function (Blueprint $table) {
+            $table->dropForeign('usuarios_perfil_id_foreign');
+            $table->dropColumn('perfil_id');
+        });
+        Schema::dropIfExists('perfis');
+    }
+}
