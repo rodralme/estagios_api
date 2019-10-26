296db0cfe90743575b91cebf27e10eebc7e23b61
Rodrigo <ti.rodrigoalmeida@gmail.com>
2019-10-26 17:26:39 -0300

    Ajuste do log

--- config/logging.php
+++ config/logging.php
@@ -36,7 +36,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['daily', 'bugsnag'],
             'ignore_exceptions' => false,
         ],
 
